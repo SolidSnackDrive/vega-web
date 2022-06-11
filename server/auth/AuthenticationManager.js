3d6092cf92d6d0a1c9b3cf51f3a3d3138f062aef
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-06-11 11:22:59 -0700

    Make docker work

--- server/auth/AuthenticationManager.js
+++ server/auth/AuthenticationManager.js
@@ -1,21 +1,21 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import {login} from '../services/LoginRequestAPI.js';
+import bodyParser from "body-parser";
+import express from "express";
+import { login } from "../services/LoginRequestAPI.js";
 
 function authModule(req, res) {
-	if (req.method == 'POST') {
-    	const userInfo = req.body;
-    	console.log(userInfo);
-    	login("http://localhost:8080/venus/authenticate", userInfo)
-    		.then(response => {
-    			console.log("Response", response);
-    			res.send(response);
-    		})
-    		.catch(error => {
-    			console.log("ERROR:", error);
-    			res.send(error);
-    		})
-    }
+  if (req.method == "POST") {
+    const userInfo = req.body;
+    console.log(userInfo);
+    login(`${process.env.API_KEY}venus/authenticate`, userInfo)
+      .then((response) => {
+        console.log("Response", response);
+        res.send(response);
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        res.send(error);
+      });
+  }
 }
 
 export default authModule;
